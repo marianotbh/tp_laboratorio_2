1dd9efc1fb0e4c77c9eba19a402a9ffae04d9cab
Mariano <mariano_d_b@hotmail.com>
2017-10-27 17:59:19 -0300

    Add files via upload

---
+++ Burgos.Mariano.2C.TP3/Excepciones/DniInvalidoException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class DniInvalidoException : Exception
+    {
+        string mensajeBase;
+
+        public DniInvalidoException()
+            :this("DNI Invalido.")
+        { }
+
+        public DniInvalidoException(Exception e)
+            :this(e.Message, e)
+        { }
+
+        public DniInvalidoException(string message)
+            :base(message)
+        {
+            this.mensajeBase = message;
+        }
+
+        public DniInvalidoException(string message, Exception e)
+            :base(message, e)
+        { }
+    }
+}
